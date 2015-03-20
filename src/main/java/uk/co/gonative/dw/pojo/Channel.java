6fb3915064e3b503141a2d45196f903537df102a
Kamran Ahmad Mirza <kamran.mirza@atos.net>
2015-03-20 17:16:20 +0000

    Source Base Mule ESB

---
+++ src/main/java/uk/co/gonative/dw/pojo/Channel.java
@@ -0,0 +1,34 @@
+package uk.co.gonative.dw.pojo;
+
+public class Channel {
+	private long channelId;
+	private String channelName;
+	private String companyName;
+	private long companyId;
+	public long getChannelId() {
+		return channelId;
+	}
+	public void setChannelId(long channelId) {
+		this.channelId = channelId;
+	}
+	public String getChannelName() {
+		return channelName;
+	}
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public long getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(long companyId) {
+		this.companyId = companyId;
+	}
+	
+	
+}
